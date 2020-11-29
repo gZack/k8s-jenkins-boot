8b50a97eadf87caf023c64e3ea276488675b9b8f
zack <code.zkg@gmail.com>
2020-11-28 16:29:01 -0800

    Initial commit

---
+++ src/test/java/com/k8s/test/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.k8s.test;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
